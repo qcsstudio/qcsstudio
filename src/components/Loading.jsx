23b1b37c623ee8e8433652ea924f8da5a25c797c
Omkar Khallar <omkarkhallar64@gmail.com>
2025-02-12 15:49:48 +0530

    newsletters

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,7 +1,9 @@
 const Loading = () => {
    return (
       <>
-         <div className="w-[100%] h-[80vh]"></div>
+         <div className="w-[100%] h-[80vh]">
+            <h1>new loading </h1>
+         </div>
       </>
    )
 }
