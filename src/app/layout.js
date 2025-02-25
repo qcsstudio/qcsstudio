d4b9e5b4c13b35972d378eaaab991b2d37e792bb
Omkar Khallar <omkarkhallar64@gmail.com>
2025-02-25 16:51:13 +0530

    scripts

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import { BlogDataContextProvider } from "@/context/BlogData";
 import { PrimeReactProvider, PrimeReactContext } from "primereact/api";
 import Head from "next/head";
 import { Analytics } from '@vercel/analytics/next';
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,14 +25,36 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
+      <Head >
       <link rel="icon" type="image/png" href="/vercel.png" />
-        
+      
+      <Script
+          id="google-tag-manager"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-MM68DHXB');
+            `,
+          }}
+        />
+
       </Head>
 
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased `}
       >
+       <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-MM68DHXB"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
 
         <PrimeReactProvider>
           <BlogDataContextProvider>
